91a08c6979f7b7ecc9627a19d5f2f47ccc2e84f2
us579 <wzus579@gmail.com>
2019-07-15 22:25:39 +1000

    add script

--- Recommender.py
+++ Recommender.py
@@ -6,17 +6,17 @@ from math import sqrt
 from sklearn.metrics.pairwise import pairwise_distances
 import scipy.sparse as sp
 from scipy.sparse.linalg import svds
+import warnings
+warnings.filterwarnings('ignore')
+
 
 
 header = ['user_id','item_id','rating','timestamp']
 m_cols = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
-df = pd.read_csv('/Users/us579/Desktop/19T2/COMP9417-19T2/RecommenderSystems/data/ml-100k/u.data',sep='\t',
-                 names=header)
-df2 = pd.read_csv('/Users/us579/Desktop/19T2/COMP9417-19T2/RecommenderSystems/data/ml-100k/u.item', sep='|',
+df = pd.read_csv('data/ml-100k/u.data',sep='\t',names=header)
+df2 = pd.read_csv('data/ml-100k/u.item', sep='|',
                   encoding='latin-1',usecols=range(5),names=m_cols)
-
-
-
+print(df.head())
 
 
 
